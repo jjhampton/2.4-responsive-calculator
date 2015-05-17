24e4f6676aa3c7657f9127bdfbe70e96f341f2ca
John Hampton <john_j_hampton@yahoo.com>
2015-05-16 23:23:39 -0400

    Re-enable display as setDisplayArea function to be called by event handlers

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -7,27 +7,20 @@ var init = function () {
     var equalsButton; //for querySelector
     var clearButton; //for querySelector
     var toggleSignButton; // for querySelector
-    var displayNumber; //for querySelector
+    var displayArea; //for querySelector
     var decimalButton; //for querySelector
     var calculation = []; //Current calculation
-    var numberPressedLast = false;
-    var decimalPressedLast = false;
+    var wasNumberPressedLast = false;
+    var wasDecimalPressedLast = false;
+    var wasOperatorPressedLast;
 
 
 
-  // Variable pointing to equals-button HTML element
+  // Using querySelector to assign variables to DOM elements
   equalsButton = document.querySelector("#button-equal");
-
-  // Variable pointing to AC-button HTML element
   clearButton = document.querySelector("#button-clear");
-
-  // Variable pointing to display-digits HTML element
-  displayNumber = document.querySelector(".display-digits");
-
-  // Variable pointing to button-toggle-sign HTML element
+  displayArea = document.querySelector(".display-digits");
   toggleSignButton = document.querySelector("#button-toggle-sign");
-
-  // Variable pointing to button-toggle-sign HTML element
   decimalButton = document.querySelector("#button-decimal");
 
   //Event handler that adds the value of the clicked number button to the calculation
@@ -35,16 +28,18 @@ var init = function () {
     var button = event.target;
     var text = button.textContent;
 
-    if (decimalPressedLast || numberPressedLast) {
-      calculation[calculation.length-1] += text;
-      //displayNumber.innerText +=  calculation[calculation.length-2] + calculation[calculation.length-1];
+    if (wasDecimalPressedLast || wasNumberPressedLast) {
+      calculation[calculation.length-1] += text; //Sets last value in array
+      setDisplayArea(calculation[calculation.length-1], false); //Displays it to screen
     }
     else {
       calculation.push(text);
+      setDisplayArea(text, false);
       //displayNumber.innerText = text;
     }
 
-    numberPressedLast = true;
+    wasNumberPressedLast = true;
+
     console.log(text + " CLICKED");
     console.log(calculation);
   };
@@ -54,15 +49,15 @@ var init = function () {
     var button = event.target;
     var text = button.textContent;
 
-    if (numberPressedLast) {
+    if (wasNumberPressedLast) {
       calculation.push(text);
     }
     else {
       calculation[calculation.length - 1] = text;
     }
 
-    numberPressedLast = false;
-    decimalPressedLast = false;
+    wasNumberPressedLast = false;
+    wasDecimalPressedLast = false;
     console.log(text + " CLICKED");
     console.log(calculation);
   };
@@ -73,14 +68,54 @@ var init = function () {
     var text = button.textContent;
     var answer;
 
-    calculation = calculation.join('');
-    answer = eval(calculation);
-
-    //displayNumber.innerText = answer;
+    answer = eval(calculation.join('')); //Evaluates stringified version of calculation array as if it were an expression
+    console.log(answer);
+    setDisplayArea(answer, false);
 
     console.log(text + " CLICKED");
-    console.log(calculation);
     console.log("answer is: " + answer);
+  };
+
+  //Event handler that toggles the sign of the last pressed number button value
+  var toggleSignPressed = function(event) {
+    var button = event.target;
+    var lastValueEntered;
+
+
+    if (wasNumberPressedLast) {
+      lastValueEntered = calculation[calculation.length - 1];
+      lastOperatorEntered = calculation[calculation.length - 2];
+      if (lastOperatorEntered === "-") {
+        calculation[calculation.length - 2] = "+";
+        setDisplayArea(lastValueEntered * -1, false);
+        console.log("Toggle sign pressed");
+        console.log(calculation);
+      }
+      else {
+        calculation[calculation.length - 1] = ( lastValueEntered * -1);
+        //displayNumber.innerText = calculation[calculation.length - 1];
+        setDisplayArea(calculation[calculation.length-1], false);
+
+        console.log("Toggle sign pressed");
+        console.log(calculation);
+      }
+    }
+  };
+
+  //Event handler that toggles the sign of the last pressed number button value
+  var decimalPressed = function(event) {
+    var button = event.target;
+    var text = button.textContent;
+    var lastValueEntered;
+
+    if (wasDecimalPressedLast===false) {
+      calculation[calculation.length - 1] += ".";
+      setDisplayArea(".", true);
+      wasDecimalPressedLast = true;
+      wasNumberPressedLast = false;
+      console.log("Decimal pressed");
+      console.log(calculation);
+    }
   };
 
   //Event handler that resets the calculation and calculator display to zero
@@ -90,40 +125,20 @@ var init = function () {
 
     calculation = [];
     //displayNumber.innerText = "0";
-    numberPressedLast = false;
-    decimalPressedLast = false;
+    setDisplayArea(0, false);
+    wasNumberPressedLast = false;
+    wasDecimalPressedLast = false;
     console.log(text + " CLICKED");
     console.log(calculation);
   };
 
-  //Event handler that toggles the sign of the last pressed number button value
-  var toggleSignPressed = function(event) {
-    var button = event.target;
-    var lastValueEntered;
-
-    lastValueEntered = calculation[calculation.length - 1];
-
-    calculation[calculation.length - 1] = ( lastValueEntered * -1);
-    //displayNumber.innerText = calculation[calculation.length - 1];
-
-    console.log("Toggle sign pressed");
-    console.log(calculation);
-  };
-
-  //Event handler that toggles the sign of the last pressed number button value
-  var decimalPressed = function(event) {
-    var button = event.target;
-    var text = button.textContent;
-    var lastValueEntered;
-
-    if (decimalPressedLast===false) {
-      calculation[calculation.length - 1] += ".";
-
-      //displayNumber.innerText += ".";
-      decimalPressedLast = true;
-      numberPressedLast = false;
-      console.log("Decimal pressed");
-      console.log(calculation);
+  //Function that sets value of calculator display area
+  var setDisplayArea = function(inputValueFromButton, shouldConcat) {
+    if (shouldConcat) {
+      displayArea.innerText += inputValueFromButton;
+    }
+    else {
+      displayArea.innerText = inputValueFromButton;
     }
   };
 
